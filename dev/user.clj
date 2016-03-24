c122fc943428fe345812903c8036d5c8d1e80d33
Chris O'Donnell <chris.bowdoin@gmail.com>
2016-03-23 21:52:19 -0400

    Add requires to user namespace.

--- dev/user.clj
+++ dev/user.clj
@@ -1,6 +1,10 @@
 (ns user
   (:require [reloaded.repl :refer [system init start stop go reset reset-all]]
-            [difficulty-api.system :refer [dev-system]]))
+            [difficulty-api.system :refer [dev-system test-system]]
+            [difficulty-api.db :as db]
+            [difficulty-api.dispatch :as dispatch]
+            [difficulty-api.torn-api :as api]
+            [com.stuartsierra.component :as component]))
 
 (reloaded.repl/set-init! #(dev-system {:db-uri "datomic:mem://difficulty-api"}))
 
